84d68cb32b9f63647982f40358db72bcf104e5f5
slumsk8 <aragaocosta85@gmail.com>
2019-11-26 16:00:16 -0300

    Adicionado metodo para deletar um produto pelo seu id, na page home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserAuthService } from 'src/app/services/auth.service';
+import { ProductService } from 'src/app/services/product.service';
+import { runInThisContext } from 'vm';
+import { Product } from 'src/app/interfaces/product';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +12,37 @@ import { UserAuthService } from 'src/app/services/auth.service';
 })
 export class HomePage implements OnInit {
 
+  public products = new Array<Product>()
+  private productSubscription: Subscription
+
   constructor(
-    private authService: UserAuthService
-  ) { }
+    private authService: UserAuthService,
+    private productService: ProductService
+  ) {
+    this.productSubscription = this.productService.getProducts()
+      .subscribe(
+        data => {
+          this.products = data
+        }
+      )
+  }
 
   ngOnInit() {
   }
-
-  logout(){
+  //destruindo "sessão" para não ficar buscando dados eternamente
+  ngOnDestroy() {
+    this.productSubscription.unsubscribe()
+  }
+  logout() {
     this.authService.logout()
+  }
+  //método para deletar o produto
+  async deleteProdudct(id: string) {
+    try {
+      await this.productService.deleteProduct(id)
+    } catch (error) {
+      console.log("Erro: " + error)
+    }
   }
 
 }
