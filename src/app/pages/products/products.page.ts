75259dc73d810cab020a8583b0dc36bb5c5fe3c5
slumsk8 <aragaocosta85@gmail.com>
2019-11-26 15:49:58 -0300

    Adicionados metodo para salvar um novo produto e metodo para fazer update

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { ProductService } from 'src/app/services/product.service';
+import { UserAuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPage implements OnInit {
 
-  constructor() { }
+  public product: Product = {}
+  private productId: string = null
+
+  constructor(
+    private productService: ProductService,
+    private userAuth: UserAuthService
+  ) { }
+
+  async addProduct() {
+    this.product.userId = this.userAuth.getAuth().currentUser.uid
+    if (this.productId) {
+      try {
+        await this.productService.updateProduct(this.product, this.productId)
+      } catch (error) {
+        console.log('Erro: ' + error)
+      }
+    } else {
+      try {
+        await this.productService.addProduct(this.product)
+      } catch (error) {
+        console.log('Erro: ' + error)
+      }
+    }
+  }
 
   ngOnInit() {
   }
